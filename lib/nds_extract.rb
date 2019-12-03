b13d670babbe07007b8933d96f4eedb154aca5a7
Daniel Liu <danliu277@gmail.com>
2019-12-03 20:20:20 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -101,20 +101,6 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
-  [
-    { :name => "Byron Poodle", 
-      :movies => [
-        { :title => "At the park" },
-        { :title => "On the couch" },
-      ]
-    },
-    { :name => "Nancy Drew", 
-      :movies => [
-        { :title => "Biting" },
-      ]
-    }
-  ] 
-  
   result = []
   index1 = 0
   index2 = 0
